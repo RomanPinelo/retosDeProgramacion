f2948fedb681254853bcbab966574867340338f3
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-11-21 15:12:22 -0600

    Reto #1 Completado

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var reto1Button = document.getElementById("reto1Button");
 var retoContenedor = document.querySelectorAll(".retoContenedor");
 var borrarButton = document.querySelectorAll(".borrarResultados");
+var mostrarButton = document.querySelectorAll(".mostrarResultados");
 
 function borrarResultado(contenedor) {
   retoContenedor[contenedor].innerHTML = "";
 }
 
-reto1Button.addEventListener("click", () => {
+// Reto #0
+mostrarButton[0].addEventListener("click", () => {
   retoContenedor[0].innerHTML = "";
   for(let i = 0; i < 100; i++) {
     if (((i + 1) % 3 == 0) && ((i + 1) % 5 == 0)) {
@@ -23,4 +24,36 @@ reto1Button.addEventListener("click", () => {
 
 borrarButton[0].addEventListener("click", () => {
   borrarResultado(0);
+});
+
+// Reto #1
+function isAnagrama(primerPalabra, segundaPalabra) {
+  let primerPalabraArray = primerPalabra.toLowerCase().split("").sort().join("");
+  let segundaPalabraArray = segundaPalabra.toLowerCase().split("").sort().join("");
+
+  console.log(primerPalabraArray, segundaPalabraArray);
+  primerPalabraArray === segundaPalabraArray ? retoContenedor[1].innerHTML = "Es un anagrama" : retoContenedor[1].innerHTML = "No es un anagrama";
+}
+
+mostrarButton[1].addEventListener("click", () => {
+  retoContenedor[1].innerHTML = "";
+
+  let primerPalabra = document.getElementById("palabra1").value.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  let segundaPalabra = document.getElementById("palabra2").value.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+  if (primerPalabra.length == 0 || segundaPalabra.length == 0) {
+    retoContenedor[1].innerHTML = "Ingresa otra palabra";
+  } else if (primerPalabra.length != segundaPalabra.length) {
+    retoContenedor[1].innerHTML = "No es un anagrama";
+  } else if(primerPalabra.toLowerCase() === segundaPalabra.toLowerCase()) {
+    retoContenedor[1].innerHTML = "No es un anagrama";
+  } else {
+    isAnagrama(primerPalabra, segundaPalabra);
+  }
+});
+
+borrarButton[1].addEventListener("click", () => {
+  document.getElementById("palabra1").value = "";
+  document.getElementById("palabra2").value = "";
+  borrarResultado(1);
 });

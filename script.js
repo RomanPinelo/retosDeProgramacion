3adf43d62f7964732decca8c21b3cda1d189fb88
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-11-21 17:08:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -37,12 +37,8 @@ function isAnagrama(primerPalabra, segundaPalabra) {
 mostrarButton[1].addEventListener("click", () => {
   retoContenedor[1].innerHTML = "";
 
-  console.log(document.getElementById("palabra1").value, document.getElementById("palabra2").value);
-
   let primerPalabra = document.getElementById("palabra1").value.normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
   let segundaPalabra = document.getElementById("palabra2").value.normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
-
-  console.log(primerPalabra, segundaPalabra);
 
   if (primerPalabra.length == 0 && segundaPalabra.length == 0) {
     retoContenedor[1].innerHTML = "Ingresa las palabras";
